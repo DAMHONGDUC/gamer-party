448b79ac2bf21be4b17d2d940141f77279f083bf
Dam Hong Duc <hongduc001h@gmail.com>
2023-02-15 18:32:14 +0700

    add absolute path

---
+++ src/screens/home/HomeScreen.tsx
@@ -0,0 +1,11 @@
+import {Text, View} from 'react-native';
+
+function HomeScreen(): JSX.Element {
+  return (
+    <View>
+      <Text>hello</Text>
+    </View>
+  );
+}
+
+export default HomeScreen;

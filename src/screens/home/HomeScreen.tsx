d9e5236ba62d3f4647e1e35a43a95e659ec6659a
Dam Hong Duc <hongduc001h@gmail.com>
2023-02-15 17:25:00 +0700

    add eslint

---
+++ src/screens/home/HomeScreen.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import React from "react";
+
+export default function HomeScreen(): JSX.Element {
+  return (
+    <View>
+      <Text>hello</Text>
+    </View>
+  );
+}
